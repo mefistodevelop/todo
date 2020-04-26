701555cee05d11c2fb70cb5c7f8e1dfa3633ddb8
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-26 23:15:45 +0300

    feat: add About component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.scss';
+
+const About = (props) => (
+  <div className="about">
+    <div className="jumbotron">
+      <h1 className="display-4 about__title">Todo List App</h1>
+      <p className="lead">App for your notes and tasks.</p>
+      <p>
+        Created by Oleg Rudenko&nbsp;
+      <a className="badge badge-primary" href="mailto:oleg.ru93@yandex.ru">oleg.ru93@yandex.ru</a>
+      </p>
+    </div>
+  </div>
+);
+
+export default About;
