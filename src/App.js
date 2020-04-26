68aa8e887ab2483e7a31773f0da9e45a6facfd04
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-26 23:13:30 +0300

    feat: create main structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './App.scss';
+import { Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <Navbar />
+      </header>
+
+      <div className="App__container">
+        <Route path="/home" render={() => <Home />} />
+        <Route path="/about" render={() => <About />} />
+      </div>
     </div>
   );
 }
