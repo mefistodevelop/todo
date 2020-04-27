bb9ad59a896c97c9f7e61712369af0846985f8e4
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-27 19:01:04 +0300

    feat: add animation for notes

--- src/components/Notes/Notes.jsx
+++ src/components/Notes/Notes.jsx
@@ -1,34 +1,40 @@
 import React, { useContext } from 'react';
 import './Notes.scss';
 import AlertContext from '../../context/alert/alertContext';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Notes = ({ notes, onRemove }) => {
   const alert = useContext(AlertContext);
   const handleRemove = (id) => {
     onRemove(id).then(() => alert.showAlert('The note has removed', 'warning'));
   };
+
   const notesItems = notes.map((note) => (
-    <li className="list-group-item notes__item" key={note.id}>
-      <div>
-        <b className="notes__item-title">
-          {note.title}
-        </b>
-        <small>{note.date}</small>
-      </div>
-      <button
-        type="button"
-        className="btn btn-outline-danger btn-sm"
-        onClick={() => handleRemove(note.id)}
+    <CSSTransition key={note.id} classNames="note" timeout={800}>
+      <li className="list-group-item notes__item">
+        <div>
+          <b className="notes__item-title">
+            {note.title}
+          </b>
+          <small>{note.date}</small>
+        </div>
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm"
+          onClick={() => handleRemove(note.id)}
         >
           &times;
         </button>
-    </li>
+      </li>
+    </CSSTransition>
+
   ));
   return (
     <div className="notes">
-      <ul className="list-group">
+      <TransitionGroup component="ul" className="list-group">
         {notes.length ? notesItems : 'There are no notes yet...'}
-      </ul>
+      </TransitionGroup>
+
     </div>
   );
 }
