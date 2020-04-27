b8fc576a331b2a8969db3e495892dbf27ea2204a
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-27 18:19:17 +0300

    feat: add animation for alert

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,25 +1,37 @@
 import React, { useContext } from 'react';
 import './Alert.scss';
 import AlertContext from '../../context/alert/alertContext';
+import { CSSTransition } from 'react-transition-group';
 
 const Alert = () => {
   const { alert, hideAlert } = useContext(AlertContext);
-  if (!alert.isVisible) return null;
 
   return (
-    <div className="Alert">
-      <div className={`alert alert-${alert.type || 'primary'} Alert__message`}>
-        {alert.text}
-        <button
-          type="button"
-          className="close"
-          aria-label="Close"
-          onClick={hideAlert}
-        >
-          <span aria-hidden="true">&times;</span>
-        </button>
+    <CSSTransition
+      in={alert.isVisible}
+      timeout={{
+        enter: 500,
+        exit: 300,
+      }}
+      classNames="alert"
+      mountOnEnter
+      unmountOnExit
+    >
+      <div className="Alert">
+        <div className={`alert alert-${alert.type || 'primary'} Alert__message`}>
+          {alert.text}
+          <button
+            type="button"
+            className="close"
+            aria-label="Close"
+            onClick={hideAlert}
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
       </div>
-    </div>
+    </CSSTransition>
+
   );
 }
 
