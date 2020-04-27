1895f9520c75a607f0b30d4d3ecdb2b9750d88c8
Oleg Rudenko <oleg.ru93@yandex.ru>
2020-04-27 13:55:31 +0300

    feat: add Spinner component

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Spinner = () => (
+  <div className="spinner">
+    <div className="spinner-border text-primary" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
+  </div>
+);
+
+export default Spinner;
